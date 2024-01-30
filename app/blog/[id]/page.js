629322d2d8040bcacdcc5bb7f0375efb55b41fcd
Rithonn <rithonn21@gmail.com>
2024-01-29 20:38:02 -0500

    Have dynamic routes loading correctly with server approach

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -1,3 +1,27 @@
-export default function Page({params}) {
-    return <div>My Post: {params.id}</div>
-}
+import Head from "next/head";
+import Date from "@/components/date";
+import { getPostData } from "@/lib/posts";
+import utilStyles from '../../../styles/utils.module.css';
+import Link from "next/link";
+
+export default async function Page({ params }) {
+    const postData = await getPostData(params.id);
+
+    return (
+      <div>
+        <Head>
+          <title>{postData.title}</title>
+        </Head>
+        <article>
+          <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+          <div className={utilStyles.lightText}>
+            <Date dateString={postData.date} />
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        </article>
+        <footer>
+            <Link href="/">Go Home</Link>
+        </footer>
+      </div>
+    );
+  }
